2945755b28578a2f67703ae13eda9ebd26de4e24
TsutomuTomizawa <tomizawa@hellowiseegg.com>
2025-08-24 14:56:55 +0900

    Fix Terraform outputs to use data sources instead of resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "service_url" {
   description = "Cloud Run service URL"
-  value       = google_cloud_run_v2_service.main.uri
+  value       = data.google_cloud_run_v2_service.main.uri
 }
 
 output "deploy_sa_email" {
   description = "Deploy service account email"
-  value       = google_service_account.github_actions.email
+  value       = data.google_service_account.github_actions.email
 }
